21da8e65eac640944cde8e53b4349dfd30597473
nagendra0032 <nagendrakumar0032@gmail.com>
2020-06-10 17:19:07 +0530

    blog card changed

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,8 +2,9 @@ import React from 'react';
 import {Link} from 'gatsby';
 import './Post.css';
 import Img from 'gatsby-image';
+import {slugify} from '../../util/UtilityFunctions'
 
-const Post = ({title, author, path, date, body, fluid}) => {
+const Post = ({title, author, slug, date, body, fluid, tags}) => {
     return(
         <div>
             <Img fluid = {fluid}/>
@@ -14,7 +15,19 @@ const Post = ({title, author, path, date, body, fluid}) => {
                 </h4>
                 <p className = "date">{date} </p>
                 <p className = "author">{author} </p>
-             <Link to={path}>Read more</Link>
+                <p>{body}</p>
+                <p>
+                    {
+                        tags.map(tag => (
+                            <li>
+                                <Link to = {`/tag/${slugify(tag)}`}>
+                                    {tag}
+                                </Link>
+                            </li>
+                        ))
+                    }
+                </p>
+             <Link to={slug}>Read more</Link>
             </div>
             
            
